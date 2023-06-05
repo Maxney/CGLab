9e8cafc800b5424ab3f73c2f3a533f4c513a0a8a
GUA <Maxney2016@yandex.ru>
2023-06-05 22:59:09 +0300

    Добавьте файлы проекта.

---
+++ CGLab/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace CGLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Color = new System.Windows.Forms.Button();
+            this.Width = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Width)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width";
+            // 
+            // Color
+            // 
+            this.Color.BackColor = System.Drawing.Color.White;
+            this.Color.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Color.ForeColor = System.Drawing.Color.White;
+            this.Color.Location = new System.Drawing.Point(116, 47);
+            this.Color.Name = "Color";
+            this.Color.Size = new System.Drawing.Size(75, 23);
+            this.Color.TabIndex = 0;
+            this.Color.TabStop = false;
+            this.Color.UseVisualStyleBackColor = false;
+            this.Color.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Width
+            // 
+            this.Width.Location = new System.Drawing.Point(116, 12);
+            this.Width.Name = "Width";
+            this.Width.Size = new System.Drawing.Size(120, 22);
+            this.Width.TabIndex = 5;
+            this.Width.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Width);
+            this.Controls.Add(this.Color);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Width)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Color;
+        private System.Windows.Forms.NumericUpDown Width;
+    }
+}
+
---
+++ CGLab/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CGLab
+{
+    public partial class Form1 : Form
+    {
+        private Point _startPoint;
+        private Pen _pen;
+        private bool _mouseButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MouseDown += Mouse_Down;
+            MouseUp += Mouse_Up;
+            MouseMove+= DrawStraight;
+        }
+
+        private void Color_Click(object sender, EventArgs e)
+        {
+
+            ColorDialog MyDialog = new ColorDialog();
+
+            MyDialog.AllowFullOpen = false;
+
+            MyDialog.ShowHelp = true;
+
+            MyDialog.Color = Width.ForeColor;
+
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                Color.BackColor = MyDialog.Color;
+                Color.ForeColor = MyDialog.Color;
+            }
+        }
+
+
+        private void Mouse_Down(object sender, MouseEventArgs e)
+        {
+            _pen = new Pen(Color.BackColor, (int)Width.Value);
+            _startPoint = e.Location;
+            _mouseButton = true;
+        }
+
+        private void Mouse_Up(object sender, MouseEventArgs e)
+        {
+            _startPoint = MousePosition;
+            _mouseButton = false;
+        }
+
+        private void DrawStraight(object sender, MouseEventArgs e)
+        {
+            if (!_mouseButton) return;
+            var graph = this.CreateGraphics();
+            graph.Clear(System.Drawing.Color.FromArgb(255, 227, 227, 227));
+            Point startPoint = new Point(_startPoint.X, _startPoint.Y);
+            Point endPoint = new Point(e.Location.X, e.Location.Y);
+            int x = startPoint.X;
+            int y = startPoint.Y;
+            int Dx = endPoint.X - startPoint.X;
+            int Dy = endPoint.Y - startPoint.Y;
+            int er = 2 * Dy - Dx;
+            for (int i = 1; i < Dx; i++)
+            {
+                graph.FillRectangle(_pen.Brush, x, y, 1, 1);
+
+                if (er >= 0)
+                {
+                    y++;
+                    er -= 2 * Dx;
+                }
+                x++;
+                er += 2 * Dy;
+            }
+        }
+    }
+}
